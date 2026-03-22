379109c9941b1c37f2c98d52e58b74c48a7700e8
Amisha <08amisha2003@gmail.com>
2026-03-22 13:39:54 +0530

    push

--- app.js
+++ app.js
@@ -14,13 +14,18 @@ app.use(bodyParser.json());
 
 // database connection
 mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("MongoDB connected successfully!");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+  .connect(process.env.MONGODB_URL,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+}).then(() =>{
+    console.log("Db is connected successfully");
+}).catch((error) =>{
+    console.log("Db is not Connected");
+    console.error(error);
+    process.exit(1);
+});
+// mongoose.connect('mongodb://mongodb:27017/my_database');
+// console.log()
 
 // Routes
 app.use("/auth", authRoutes);
@@ -28,7 +33,7 @@ app.use("/products", productRoutes);
 app.use("/cart", cartRoutes);
 
 // port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
